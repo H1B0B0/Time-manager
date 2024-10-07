92762fe8c1ce97d6263ba083aa044064a6f01e70
H1B0B0 <hibosmurf@gmail.com>
2024-10-07 11:57:08 +0200

    Add Ecto migrations for users, clocks, and working times

--- backend/test/support/fixtures/times_fixtures.ex
+++ backend/test/support/fixtures/times_fixtures.ex
@@ -33,4 +33,34 @@ defmodule Backend.TimesFixtures do
 
     working_time
   end
+
+  @doc """
+  Generate a clock.
+  """
+  def clock_fixture(attrs \\ %{}) do
+    {:ok, clock} =
+      attrs
+      |> Enum.into(%{
+        status: true,
+        time: ~U[2024-10-06 09:55:00Z]
+      })
+      |> Backend.Times.create_clock()
+
+    clock
+  end
+
+  @doc """
+  Generate a working_time.
+  """
+  def working_time_fixture(attrs \\ %{}) do
+    {:ok, working_time} =
+      attrs
+      |> Enum.into(%{
+        end: ~U[2024-10-06 09:55:00Z],
+        start: ~U[2024-10-06 09:55:00Z]
+      })
+      |> Backend.Times.create_working_time()
+
+    working_time
+  end
 end
