77f54f328a4a749813e47227b2f5682cb543947e
H1B0B0 <hibosmurf@gmail.com>
2024-10-10 10:29:52 +0200

    Refactor App.vue to include NavBar and User components

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import User from "./components/User.vue";
+import NavBar from "./components/NavBar.vue";
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <header class="w-full"></header>
+    <main class="flex-grow">
+      <NavBar />
+      <User />
+    </main>
+  </div>
+</template>
