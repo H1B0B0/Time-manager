7a384aa2af03a69356aea5244c137ce1d881d171
NathanMunsch <nathan.munsch@epitech.eu>
2024-10-12 20:29:58 +0200

    feat: Add logout button in navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -26,9 +26,9 @@
                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
             </li>
             <li>
-              <a href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                out</a>
+                <router-link to="/" @click="logout"
+                class="block w-full text-left px-4 py-2 text-sm text-gray-200 hover:bg-gray-600 hover:text-white">Sign
+                out</router-link>
             </li>
           </ul>
         </div>
@@ -88,9 +88,14 @@ export default {
       email.value = newUser?.email ?? '';
     });
 
+    const logout = () => {
+      userStore.setUser(null);
+    };
+
     return {
       username,
-      email
+      email,
+      logout
     };
   }
 };
