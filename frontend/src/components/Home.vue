a7d4a19923dbc034893c0b3a0e9a8c85d451a8d8
H1B0B0 <hibosmurf@gmail.com>
2024-10-14 17:10:50 +0200

    refactor: Add animejs dependency and implement scroll-triggered animations

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -112,9 +112,10 @@
         </div>
       </div>
     </div>
-    <div class="flex flex-wrap gap-8 justify-center align-middle mb-40">
+    <div class="flex flex-wrap gap-8 justify-center align-middle mb-20">
       <div
-        class="bg-gray-800 p-6 rounded-lg flex flex-col items-center w-full md:w-5/12"
+        class="bg-gray-800 p-6 rounded-lg flex flex-col items-center w-full md:w-5/12 opacity-0"
+        id="chart1"
       >
         <h2 class="text-xl mb-4">Project Time Utilization</h2>
         <div class="flex justify-between text-center mb-4 w-full">
@@ -135,24 +136,117 @@
       </div>
 
       <div
-        class="bg-gray-800 p-6 rounded-lg flex flex-col items-center w-full md:w-5/12"
+        class="bg-gray-800 p-6 rounded-lg flex flex-col items-center w-full md:w-5/12 opacity-0"
+        id="chart2"
       >
         <h2 class="text-xl mb-4">Productivity Trends</h2>
         <canvas id="horizontalBarChart" class="w-full"></canvas>
       </div>
 
       <div
-        class="bg-gray-800 p-6 rounded-lg flex flex-col items-center w-full md:w-5/12"
+        class="bg-gray-800 p-6 rounded-lg flex flex-col items-center w-full md:w-5/12 opacity-0"
+        id="chart3"
       >
         <h2 class="text-xl mb-4">Monthly Time Distribution</h2>
         <canvas id="barChart" class="w-full"></canvas>
       </div>
 
       <div
-        class="bg-gray-800 p-6 rounded-lg flex flex-col items-center w-full md:w-5/12"
+        class="bg-gray-800 p-6 rounded-lg flex flex-col items-center w-full md:w-5/12 opacity-0"
+        id="chart4"
       >
         <h2 class="text-xl mb-4">Tasks Over Time</h2>
         <canvas id="areaChart" class="w-full"></canvas>
+      </div>
+    </div>
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 mb-40" id="avis">
+      <div
+        class="relative overflow-hidden bg-gray-900 px-6 py-20 shadow-xl sm:rounded-3xl sm:px-10 sm:py-24 md:px-12 lg:px-20"
+      >
+        <img
+          class="absolute inset-0 h-full w-full object-cover brightness-150 saturate-0"
+          src="https://images.unsplash.com/photo-1601381718415-a05fb0a261f3?ixid=MXwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8ODl8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1216&q=80"
+          alt=""
+        />
+        <div class="absolute inset-0 bg-gray-900/90 mix-blend-multiply" />
+        <div
+          class="absolute -left-80 -top-56 transform-gpu blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-[0.45]"
+            style="
+              clip-path: polygon(
+                74.1% 44.1%,
+                100% 61.6%,
+                97.5% 26.9%,
+                85.5% 0.1%,
+                80.7% 2%,
+                72.5% 32.5%,
+                60.2% 62.4%,
+                52.4% 68.1%,
+                47.5% 58.3%,
+                45.2% 34.5%,
+                27.5% 76.7%,
+                0.1% 64.9%,
+                17.9% 100%,
+                27.6% 76.8%,
+                76.1% 97.7%,
+                74.1% 44.1%
+              );
+            "
+          />
+        </div>
+        <div
+          class="hidden md:absolute md:bottom-16 md:left-[50rem] md:block md:transform-gpu md:blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-25"
+            style="
+              clip-path: polygon(
+                74.1% 44.1%,
+                100% 61.6%,
+                97.5% 26.9%,
+                85.5% 0.1%,
+                80.7% 2%,
+                72.5% 32.5%,
+                60.2% 62.4%,
+                52.4% 68.1%,
+                47.5% 58.3%,
+                45.2% 34.5%,
+                27.5% 76.7%,
+                0.1% 64.9%,
+                17.9% 100%,
+                27.6% 76.8%,
+                76.1% 97.7%,
+                74.1% 44.1%
+              );
+            "
+          />
+        </div>
+        <div class="relative mx-auto max-w-2xl lg:mx-0">
+          <img
+            class="h-12 w-auto"
+            src="https://tailwindui.com/plus/img/logos/workcation-logo-white.svg"
+            alt=""
+          />
+          <figure>
+            <blockquote
+              class="mt-6 text-lg font-semibold text-white sm:text-xl sm:leading-8"
+            >
+              <p>
+                “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                expedita voluptas culpa sapiente alias molestiae. Numquam
+                corrupti in laborum sed rerum et corporis.”
+              </p>
+            </blockquote>
+            <figcaption class="mt-6 text-base text-white">
+              <div class="font-semibold">Judith Black</div>
+              <div class="mt-1">CEO of Workcation</div>
+            </figcaption>
+          </figure>
+        </div>
       </div>
     </div>
   </div>
@@ -173,6 +267,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import anime from "animejs/lib/anime.es.js";
 
 Chart.register(
   LineElement,
@@ -190,10 +285,31 @@ Chart.register(
 export default {
   name: "Dashboard",
   setup() {
+    const animateOnScroll = (element, animationIn, animationOut) => {
+      let threshold = 0.3;
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              threshold = 0.3;
+              animationIn.play();
+              animationOut.pause();
+            } else {
+              threshold = 0;
+              animationOut.play();
+              animationIn.pause();
+            }
+          });
+        },
+        { threshold: threshold }
+      );
+      observer.observe(element);
+    };
+
     onMounted(() => {
       // Line Chart for card 1
       const lineCtx = document.getElementById("lineChart").getContext("2d");
-      new Chart(lineCtx, {
+      const lineChart = new Chart(lineCtx, {
         type: "line",
         data: {
           labels: [
@@ -222,7 +338,7 @@ export default {
       const horizontalBarCtx = document
         .getElementById("horizontalBarChart")
         .getContext("2d");
-      new Chart(horizontalBarCtx, {
+      const horizontalBarChart = new Chart(horizontalBarCtx, {
         type: "bar",
         data: {
           labels: ["Productive Time", "Neutral Time", "Unproductive Time"],
@@ -242,7 +358,7 @@ export default {
 
       // Bar Chart for card 3
       const barCtx = document.getElementById("barChart").getContext("2d");
-      new Chart(barCtx, {
+      const barChart = new Chart(barCtx, {
         type: "bar",
         data: {
           labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
@@ -261,7 +377,7 @@ export default {
 
       // Area Chart for card 4
       const areaCtx = document.getElementById("areaChart").getContext("2d");
-      new Chart(areaCtx, {
+      const areaChart = new Chart(areaCtx, {
         type: "line",
         data: {
           labels: ["Mar", "May", "Jul", "Sep", "Nov", "Jan"],
@@ -279,6 +395,54 @@ export default {
           responsive: true,
         },
       });
+
+      // Animations for the charts
+      const charts = [
+        { element: document.getElementById("chart1"), index: 0 },
+        { element: document.getElementById("chart2"), index: 2 },
+        { element: document.getElementById("chart3"), index: 1 },
+        { element: document.getElementById("chart4"), index: 3 },
+      ];
+
+      charts.forEach(({ element }, index) => {
+        const direction = index % 2 === 0 ? "left" : "right";
+        const animationIn = anime({
+          targets: element,
+          opacity: [0, 1],
+          translateX: direction === "left" ? [-100, 0] : [100, 0],
+          duration: 1500,
+          autoplay: false,
+        });
+
+        const animationOut = anime({
+          targets: element,
+          opacity: [1, 0],
+          duration: 2000,
+          autoplay: false,
+        });
+
+        animateOnScroll(element, animationIn, animationOut);
+      });
+
+      // Animation for the #avis section
+      const avisElement = document.getElementById("avis");
+      const avisIn = anime({
+        targets: avisElement,
+        opacity: [0, 1],
+        translateY: [100, 0], // Animate from bottom to top
+        duration: 1000,
+        autoplay: false,
+      });
+
+      const avisOut = anime({
+        targets: avisElement,
+        opacity: [1, 0],
+        duration: 2000,
+        autoplay: false,
+      });
+
+      // Apply scroll-triggered animation to avis section
+      animateOnScroll(avisElement, avisIn, avisOut);
     });
   },
 };
