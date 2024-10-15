dac43b5c46a35573fb7176263769d3ca26179a26
NathanMunsch <nathan.munsch@epitech.eu>
2024-10-15 16:26:53 +0200

    feat: Implement account update

--- backend/lib/backend/accounts.ex
+++ backend/lib/backend/accounts.ex
@@ -124,6 +124,13 @@ defmodule Backend.Accounts do
 
   """
   def update_user(%User{} = user, attrs) do
+    attrs =
+      if Map.has_key?(attrs, "password") do
+        Map.put(attrs, "password", Pbkdf2.hash_pwd_salt(attrs["password"]))
+      else
+        attrs
+      end
+
     user
     |> User.changeset(attrs)
     |> Repo.update()
