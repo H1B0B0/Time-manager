d933003ac3035fca8e2db024b126a4438e0531ef
Roess-Matthieu <145779209+Roess-Matthieu@users.noreply.github.com>
2024-10-17 10:28:42 +0200

    Refactor router path for admin dashboard and update NavBar.vue
    - Update the router path for the admin dashboard in the NavBar.vue component.
    - The router path is now based on the user's role.
    - If the user's role is 'Manager', the path is `/admin/dashboard/{user.id}`.
    - Otherwise, the path is `/dashboard/1`.

--- frontend/src/components/adminDashboard.vue
+++ frontend/src/components/adminDashboard.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="text-white">
-    <h1 class="text-2xl font-bold mb-4 text-center mb-2">Admin Dashboard</h1>
+    <h1 class="text-2xl font-bold mb-4 text-center">Team 1</h1>
+    <div class="backdrop-blur-2xl shadow-xl border p-6 rounded-3xl">
+      <table class="max-w-full divide-y  divide-white">
+        <thead >
+          <tr>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Id</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Username</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Email</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Role</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Schedule</th>
+          </tr>
+        </thead>
+        <tbody class="divide-y divide-white">
+          <tr v-for="user in users" :key="user.id">
+            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-300">{{ user.id }}</td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.username }}</td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.email }}</td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.role }}</td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300"><button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Modify</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <h1 class="text-2xl font-bold mb-4 mt-4 text-center">Team 2</h1>
     <div class="backdrop-blur-2xl shadow-xl border p-6 rounded-3xl">
       <table class="max-w-full divide-y  divide-white">
         <thead >
