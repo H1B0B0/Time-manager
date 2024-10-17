f7ddc01c84d1124b78c49061b8a81bf8e5a783f5
Roess-Matthieu <145779209+Roess-Matthieu@users.noreply.github.com>
2024-10-17 11:50:09 +0200

    Refactor user table layout in admin dashboard

--- frontend/src/components/adminDashboard.vue
+++ frontend/src/components/adminDashboard.vue
@@ -2,46 +2,134 @@
   <div class="text-white">
     <h1 class="text-2xl font-bold mb-4 text-center">Team 1</h1>
     <div class="backdrop-blur-2xl shadow-xl border p-6 rounded-3xl">
-      <table class="max-w-full divide-y  divide-white">
-        <thead >
+      <table class="max-w-full divide-y divide-white">
+        <thead>
           <tr>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Id</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Username</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Email</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Role</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Schedule</th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Id
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Username
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Email
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Role
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Info
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Schedule
+            </th>
           </tr>
         </thead>
         <tbody class="divide-y divide-white">
           <tr v-for="user in users" :key="user.id">
-            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-300">{{ user.id }}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.username }}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.email }}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.role }}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300"><button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Modify</button></td>
+            <td
+              class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-300 text-center"
+            >
+              {{ user.id }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              {{ user.username }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              {{ user.email }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              {{ user.role }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              <button
+                v-on:click="viewUserInfo(user.id)"
+                type="button"
+                class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                View
+              </button>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              <button
+                type="button"
+                class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Modify
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
     <h1 class="text-2xl font-bold mb-4 mt-4 text-center">Team 2</h1>
     <div class="backdrop-blur-2xl shadow-xl border p-6 rounded-3xl">
-      <table class="max-w-full divide-y  divide-white">
-        <thead >
+      <table class="max-w-full divide-y divide-white">
+        <thead>
           <tr>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Id</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Username</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Email</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Role</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Schedule</th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Id
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Username
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Email
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Role
+            </th>
+            <th
+              class="px-6 py-3 text-center text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Schedule
+            </th>
           </tr>
         </thead>
         <tbody class="divide-y divide-white">
           <tr v-for="user in users" :key="user.id">
-            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-300">{{ user.id }}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.username }}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.email }}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{{ user.role }}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300"><button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Modify</button></td>
+            <td
+              class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-300 text-center"
+            >
+              {{ user.id }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              {{ user.username }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              {{ user.email }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              {{ user.role }}
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 text-center">
+              <button
+                type="button"
+                class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Modify
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -50,13 +138,18 @@
 </template>
 
 <script>
-import { getAllUsers } from '@/functions/User';
-import { ref } from 'vue';
+import { getAllUsers } from "@/functions/User";
+import router from "@/router";
+import { ref } from "vue";
 
 export default {
-  name: 'AdminDashboard',
+  name: "AdminDashboard",
   setup() {
     const users = ref([]);
+
+    const viewUserInfo = (id) => {
+      router.push({ name: "Dashboard", params: { id: id } });
+    };
 
     getAllUsers().then((response) => {
       users.value = response.data;
@@ -64,6 +157,7 @@ export default {
 
     return {
       users,
+      viewUserInfo,
     };
   },
 };
