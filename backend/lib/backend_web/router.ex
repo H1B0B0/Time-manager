03b33fb30affbebf7eacc42211c80ddc8aaafb25
NathanMunsch <nathan.munsch@epitech.eu>
2024-10-23 20:47:32 +0200

    Refactor router.ex to add role-based authentication plugs

--- backend/lib/backend_web/router.ex
+++ backend/lib/backend_web/router.ex
@@ -9,41 +9,36 @@ defmodule BackendWeb.Router do
     plug Backend.Plugs.AuthenticationPlug
   end
 
-  scope "/api", BackendWeb do
-    pipe_through [:api, :auth]
+  pipeline :is_gm do
+    plug Backend.Plugs.IsGMPlug
+  end
 
-    get "/clocks/:user_id", ClockController, :show
-    post "/clocks/:user_id", ClockController, :create
+  pipeline :is_manager_or_gm do
+    plug Backend.Plugs.IsManagerOrGMPlug
+  end
 
-    get "/users", UserController, :index
-    get "/users/:user_id", UserController, :show
-    put "/users/:user_id", UserController, :update
-    get "/all-users", UserController, :getAllUsers
-    delete "/users/:user_id", UserController, :delete
+  pipeline :is_owner_or_gm do
+    plug Backend.Plugs.IsOwnerOrGMPlug
+  end
 
-    delete "/workingtime/:working_time_id", WorkingtimeController, :delete
-    put "/workingtime/:user_id", WorkingtimeController, :update
-    post "/workingtime/users", WorkingtimeController, :create
-    get "/workingtime/:user_id/:working_time_id", WorkingtimeController, :show
-    get "/workingtime/:user_id", WorkingtimeController, :getAll
+  pipeline :is_owner_or_team_owner_or_gm do
+    plug Backend.Plugs.IsOwnerOrTeamOwnerOrGMPlug
+  end
 
-    get "/roles", RoleController, :index
-    get "/roles/:role_id", RoleController, :show
+  pipeline :is_owner do
+    plug Backend.Plugs.IsOwnerPlug
+  end
 
-    post "/auth/user", AuthController, :user
+  pipeline :is_team_owner_or_gm do
+    plug Backend.Plugs.IsTeamOwnerOrGMPlug
+  end
 
-    post "/user/:user_id/role/:role_id", UserRoleController, :update
+  pipeline :create_is_team_owner_or_gm do
+    plug Backend.Plugs.Workingtime.CreateIsTeamOwnerOrGMPlug
+  end
 
-    post "/user/:user_id/team/:team_id", TeamUserController, :create
-    delete "/user/:user_id/team/:team_id", TeamUserController, :delete
-    get "/user/:user_id/teams", TeamUserController, :show
-    get "/team/:team_id/users", TeamUserController, :show
-
-    get "/teams", TeamController, :index
-    get "/teams/:team_id", TeamController, :show
-    post "/teams", TeamController, :create
-    put "/teams/:team_id", TeamController, :update
-    delete "/teams/:team_id", TeamController, :delete
+  pipeline :delete_is_team_owner_or_gm do
+    plug Backend.Plugs.Workingtime.DeleteIsTeamOwnerOrGMPlug
   end
 
   scope "/api", BackendWeb do
@@ -51,6 +46,84 @@ defmodule BackendWeb.Router do
 
     post "/auth/login", AuthController, :login
     post "/users", UserController, :create
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth]
+
+    get "/roles", RoleController, :index
+    get "/roles/:role_id", RoleController, :show
+
+    post "/auth/user", AuthController, :user
+
+    get "/user/:user_id/teams", TeamUserController, :show
+    get "/team/:team_id/users", TeamUserController, :show
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth, :is_gm]
+
+    post "/user/:user_id/role/:role_id", UserRoleController, :update
+
+    get "/teams", TeamController, :index
+    post "/teams", TeamController, :create
+    put "/teams/:team_id", TeamController, :update
+    delete "/teams/:team_id", TeamController, :delete
+
+    post "/user/:user_id/team/:team_id", TeamUserController, :create
+    delete "/user/:user_id/team/:team_id", TeamUserController, :delete
+
+    get "/all-users", UserController, :getAllUsers
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth, :is_manager_or_gm]
+
+    get "/users", UserController, :index
+    get "/users/:user_id", UserController, :show
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth, :is_owner_or_gm]
+
+    delete "/users/:user_id", UserController, :delete
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth, :is_owner_or_team_owner_or_gm]
+
+    get "/clocks/:user_id", ClockController, :show
+
+    get "/workingtime/:user_id/:working_time_id", WorkingtimeController, :show
+    get "/workingtime/:user_id", WorkingtimeController, :getAll
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth, :is_owner]
+
+    post "/clocks/:user_id", ClockController, :create
+
+    put "/users/:user_id", UserController, :update
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth, :is_team_owner_or_gm]
+
+    get "/teams/:team_id", TeamController, :show
+
+    put "/workingtime/:user_id", WorkingtimeController, :update
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth, :create_is_team_owner_or_gm]
+
+    post "/workingtime/:user_id", WorkingtimeController, :create
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through [:api, :auth, :delete_is_team_owner_or_gm]
+
+    delete "/workingtime/:working_time_id", WorkingtimeController, :delete
   end
 
   if Application.compile_env(:backend, :dev_routes) do
