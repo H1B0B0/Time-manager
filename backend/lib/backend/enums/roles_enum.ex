990ee385c6bd5480a6def467ac8fcb98af9cf432
NathanMunsch <nathan.munsch@epitech.eu>
2024-10-23 20:45:43 +0200

    Add roles enum module

---
+++ backend/lib/backend/enums/roles_enum.ex
@@ -0,0 +1,9 @@
+defmodule Backend.Enums.RolesEnum do
+  @role_employee 1
+  @role_manager 2
+  @role_general_manager 3
+
+  def role_employee, do: @role_employee
+  def role_manager, do: @role_manager
+  def role_general_manager, do: @role_general_manager
+end
